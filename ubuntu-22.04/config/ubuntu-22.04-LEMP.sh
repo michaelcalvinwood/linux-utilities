34b157187579fedbd70b8ce3e68d9bf6e10f05b9
Michael Wood <michaelwood33311@icloud.com>
2023-02-08 14:03:51 -0500

    added installing nodeJS to automated ubuntu 22.04 LEMP stack

--- ubuntu-22.04/config/ubuntu-22.04-LEMP.sh
+++ ubuntu-22.04/config/ubuntu-22.04-LEMP.sh
@@ -126,5 +126,9 @@ apt install -y net-tools
 # Enable the firewall
 sudo ufw --force enable
 
+# Install nodeJS
+curl -sL https://deb.nodesource.com/setup_18.x -o nodesource_setup.sh
+sudo bash nodesource_setup.sh
+sudo apt-get install -y nodejs
 
 

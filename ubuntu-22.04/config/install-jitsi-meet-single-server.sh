c30bbf7511605c729cd37de7870431aff5f4d036
Michael Wood <michaelwood33311@icloud.com>
2023-03-06 17:42:37 -0500

    added more automation to installing jitsi meet on ubuntu 22.04

--- ubuntu-22.04/config/install-jitsi-meet-single-server.sh
+++ ubuntu-22.04/config/install-jitsi-meet-single-server.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+mkdir /home/keys
+cp treepad* /home/keys/
+chmod 0755 -r /home/keys
+
 # MultiServer setup: https://facsiaginsa.com/jitsi/install-jitsi-meet-with-multi-server-configuration
 apt install net-tools
 
@@ -9,7 +13,7 @@ sudo ufw allow 443/tcp
 sudo ufw allow 3478/udp
 sudo ufw allow 5349/tcp
 sudo ufw allow 10000/udp
-sudo ufw enable
+sudo ufw --force enable
 
 curl https://download.jitsi.org/jitsi-key.gpg.key -o jitsi-key.gpg.key
 sudo gpg --output /usr/share/keyrings/jitsi-key.gpg --dearmor jitsi-key.gpg.key
@@ -21,8 +25,19 @@ echo "deb [signed-by=/usr/share/keyrings/prosody-keyring.gpg] http://packages.pr
 
 rm jitsi-key.gpg.key prosody-debian-packages.key
 
-sudo apt update
-sudo apt install jitsi-meet
+sudo DEBIAN_FRONTEND=noninteractive apt update
+
+cat << EOF | sudo debconf-set-selections
+jitsi-videobridge   jitsi-videobridge/jvb-hostname  string  jitsi-test-02.treepadcloud.com
+jitsi-meet-prosody  jitsi-videobridge/jvb-hostname  string  jitsi-test-02.treepadcloud.com
+jitsi-meet-web-config	jitsi-meet/jaas-choice	boolean	false
+jitsi-meet-web-config   jitsi-meet/cert-choice  select I want to use my own certificate
+jitsi-meet-web-config   jitsi-meet/cert-path-crt    string  /home/keys/treepadcloud.com.pem
+jitsi-meet-web-config   jitsi-meet/cert-path-key    string  /home/keys/treepadcloud.com.key
+EOF
+
+
+sudo DEBIAN_FRONTEND=noninteractive apt install -y jitsi-meet
 
 # Complete Uninstall
 # sudo apt purge jigasi jitsi-meet jitsi-meet-web-config jitsi-meet-prosody jitsi-meet-turnserver jitsi-meet-web jicofo jitsi-videobridge2
@@ -32,7 +47,4 @@ sudo apt install jitsi-meet
 # /var/log/jitsi/jicofo.log
 # /var/log/prosody/prosody.log
 
-# Install Co-Turn
-wget http://mirrors.edge.kernel.org/ubuntu/pool/universe/c/coturn/coturn_4.5.2-3.1_amd64.deb
-sudo apt install ./coturn_4.5.2-3.1_amd64.deb
-sudo apt install jitsi-meet-turnserver
+

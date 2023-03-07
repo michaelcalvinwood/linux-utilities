d96b64ca0d202899e7c93bcd31d5884a60fee4ed
Michael Wood <michaelwood33311@icloud.com>
2023-03-06 20:49:35 -0500

    added JWT Token to Jitsi Installation for Ubuntu 22.04

--- ubuntu-22.04/config/install-jitsi-meet-single-server.sh
+++ ubuntu-22.04/config/install-jitsi-meet-single-server.sh
@@ -25,19 +25,24 @@ echo "deb [signed-by=/usr/share/keyrings/prosody-keyring.gpg] http://packages.pr
 
 rm jitsi-key.gpg.key prosody-debian-packages.key
 
-sudo DEBIAN_FRONTEND=noninteractive apt update
+sudo  apt update
 
-cat << EOF | sudo debconf-set-selections
-jitsi-videobridge   jitsi-videobridge/jvb-hostname  string  jitsi-test-02.treepadcloud.com
-jitsi-meet-prosody  jitsi-videobridge/jvb-hostname  string  jitsi-test-02.treepadcloud.com
-jitsi-meet-web-config	jitsi-meet/jaas-choice	boolean	false
-jitsi-meet-web-config   jitsi-meet/cert-choice  select I want to use my own certificate
-jitsi-meet-web-config   jitsi-meet/cert-path-crt    string  /home/keys/treepadcloud.com.pem
-jitsi-meet-web-config   jitsi-meet/cert-path-key    string  /home/keys/treepadcloud.com.key
-EOF
+# cat << EOF | sudo debconf-set-selections
+# jitsi-videobridge   jitsi-videobridge/jvb-hostname  string  jitsi-test-02.treepadcloud.com
+# jitsi-meet-prosody  jitsi-videobridge/jvb-hostname  string  jitsi-test-02.treepadcloud.com
+# jitsi-meet-web-config	jitsi-meet/jaas-choice	boolean	false
+# jitsi-meet-web-config   jitsi-meet/cert-choice  select I want to use my own certificate
+# jitsi-meet-web-config   jitsi-meet/cert-path-crt    string  /home/keys/treepadcloud.com.pem
+# jitsi-meet-web-config   jitsi-meet/cert-path-key    string  /home/keys/treepadcloud.com.key
+# EOF
 
 
-sudo DEBIAN_FRONTEND=noninteractive apt install -y jitsi-meet
+sudo apt install jitsi-meet
+
+# Install JWT Token Authentication
+sudo apt install jitsi-meet-tokens
+
+
 
 # Complete Uninstall
 # sudo apt purge jigasi jitsi-meet jitsi-meet-web-config jitsi-meet-prosody jitsi-meet-turnserver jitsi-meet-web jicofo jitsi-videobridge2

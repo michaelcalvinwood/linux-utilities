5615f875a2bdecb9997678385e4549220f9e4b91
Michael Wood <michaelwood33311@icloud.com>
2023-04-11 08:13:44 -0400

    added info on installing postgresql on Ubuntu 22.04

---
+++ ubuntu-22.04/config/install-postgresql.sh
@@ -0,0 +1,9 @@
+# https://www.digitalocean.com/community/tutorials/how-to-install-and-use-postgresql-on-ubuntu-22-04
+# remote access setup: https://blog.logrocket.com/setting-up-a-remote-postgres-database-server-on-ubuntu-18-04/
+
+
+sudo apt update
+sudo apt install postgresql postgresql-contrib
+sudo systemctl start postgresql.service
+
+

916af3890fc55efd63f813a98096c592dc877a33
Michael Wood <michaelwood33311@icloud.com>
2024-03-27 14:20:02 -0400

    updated installing haproxy

--- ubuntu-22.04/config/install-haproxy.sh
+++ ubuntu-22.04/config/install-haproxy.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+sudo apt update
+sudo apt upgrade -y
+
+
 add-apt-repository ppa:vbernat/haproxy-2.8 -y
 apt install -y haproxy=2.8.\*
 haproxy -v
@@ -9,4 +13,12 @@ systemctl status haproxy
 systemctl enable haproxy
 
 cp /etc/haproxy/haproxy.cfg /etc/haproxy/haproxy.cfg.bk
+
+apt install -y net-tools
+
+ufw allow 22/tcp
+ufw allow 80/tcp
+ufw allow 443/tcp
+
+sudo ufw --force enable
 
